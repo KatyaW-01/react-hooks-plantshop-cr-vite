14e440148eca41b4b05c48d5a6ec1e0d173f70dd
Katya Weicht <katya.weicht@gmail.com>
2025-06-11 16:13:40 -0600

    Add out of stock toggle

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { useState } from 'react'
 
 function PlantCard({plant}) {
+  const [inStock, setInStock] = useState(true)
+
+  function handleToggleStock() {
+    setInStock(prev => !prev)
+  }
+
   return (
     <li className="card" data-testid="plant-item" >
       <img src={plant.image} alt={"plant name"} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button className="primary" onClick={handleToggleStock} >In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleToggleStock} >Out of Stock</button>
       )}
     </li>
   );
