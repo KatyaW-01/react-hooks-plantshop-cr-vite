6406f2ba8f05ffed4286201771b105809c88e0dc
Katya Weicht <katya.weicht@gmail.com>
2025-06-11 14:19:55 -0600

    Implement useEffect and pass state as props to plant list

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from 'react'
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
@@ -6,10 +7,15 @@ import Search from "./Search";
 function PlantPage() {
   const [plants, setPlants] = useState(null)
 
+  useEffect(() => {
+    loadPlantData()
+  },[])
+
   async function loadPlantData() {
     try {
       const response = await fetch("http://localhost:6001/plants")
       const plantData = await response.json()
+      setPlants(plantData)
     } catch(error) {
       console.log('Failed to load plant data', error)
     }
@@ -19,7 +25,7 @@ function PlantPage() {
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} setPlants={setPlants}/>
     </main>
   );
 }
