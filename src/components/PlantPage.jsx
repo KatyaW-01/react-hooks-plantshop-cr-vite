20638a07ea754cae26e8afe8d0137242250aa654
Katya Weicht <katya.weicht@gmail.com>
2025-06-11 16:52:01 -0600

    Add filtering feature

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -5,8 +5,9 @@ import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
-  const [plants, setPlants] = useState(null)
+  const [plants, setPlants] = useState([])
   const [formData, setFormData] = useState({name: "", image: "", price: ""})
+  const [allPlants, setAllPlants] = useState([])
 
   useEffect(() => {
     loadPlantData()
@@ -17,6 +18,7 @@ function PlantPage() {
       const response = await fetch("http://localhost:6001/plants")
       const plantData = await response.json()
       setPlants(plantData)
+      setAllPlants(plantData)
     } catch(error) {
       console.log('Failed to load plant data', error)
     }
@@ -25,7 +27,7 @@ function PlantPage() {
   return (
     <main>
       <NewPlantForm formData={formData} setFormData={setFormData} setPlants={setPlants}/>
-      <Search />
+      <Search setPlants={setPlants} allPlants={allPlants}/>
       <PlantList plants={plants} />
     </main>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,34 @@
 import React from "react";
+import { useState } from 'react'
 
-function Search() {
+function Search({setPlants, allPlants}) {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  function handleChange(event) {
+    const value = event.target.value
+    setSearchTerm(value)
+
+    if(value.trim() === "") {
+      setPlants(allPlants)
+    } else {
+      const filteredPlants = allPlants.filter((plant) => {
+        return plant.name.toLowerCase().includes(value.toLowerCase())
+      })
+
+      setPlants(filteredPlants)
+    }
+    
+  }
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
         type="text"
         id="search"
+        value={searchTerm}
+        onChange={handleChange}
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
       />
     </div>
   );
