a5deebf000f286e8c20236548f96f3845582be73
Katya Weicht <katya.weicht@gmail.com>
2025-06-10 21:06:18 -0600

    Add state and function to fetch plant data

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -4,6 +4,17 @@ import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState(null)
+
+  async function loadPlantData() {
+    try {
+      const response = await fetch("http://localhost:6001/plants")
+      const plantData = await response.json()
+    } catch(error) {
+      console.log('Failed to load plant data', error)
+    }
+  }
+
   return (
     <main>
       <NewPlantForm />
